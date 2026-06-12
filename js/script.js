391e3bc1d888ea9a4e1da2f3c0f7928250a99c2a
Strangehell64 <joaovictorsantossilva4345@gmail.com>
2026-06-12 15:40:21 -0300

    inicio de back-end

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// ação de botão login
+function logbt() {
+    // variavel de usuario
+    let usuario = document.getElementById("user").value
+    // variavel de senha
+    let senha = document.getElementById("sen").value
+    
+    // condição de senha
+    if (usuario == "admin" && senha == 123456) {
+        alert("Bem Vindo Usuario")
+        window.location.href = 'dashboard.html'
+    }else {
+        alert("usuario incorreto, tente novamente");
+    }
+}
+
+function criar(){
+    
+}
