b2b54b47166359d6ba32302c9651a93241715ce4
Strangehell64 <joaovictorsantossilva4345@gmail.com>
2026-07-03 16:44:54 -0300

    ajuste-javascript

--- js/script.js
+++ js/script.js
@@ -4,16 +4,53 @@ function logbt() {
     let usuario = document.getElementById("user").value
     // variavel de senha
     let senha = document.getElementById("sen").value
-    
+
     // condição de senha
     if (usuario == "admin" && senha == 123456) {
         alert("Bem Vindo Usuario")
         window.location.href = 'dashboard.html'
-    }else {
+    } else {
         alert("usuario incorreto, tente novamente");
     }
 }
-
-function criar(){
-    
+// variavel modal
+const modal = document.querySelector(".container-modal");
+// moldura modal
+const boxmodal = document.querySelector(".modal");
+// botão modal fechar
+const cancel = document.querySelector("#cancel")
+// criar tarefa
+const criar = document.querySelectorAll("#adicao")
+// Ação do modal
+function criar() {
+    // estilo para modal
+    modal.style.display = "flex";
 }
+// esconder modal
+function cancel() {
+    modal.style.display = "none";
+}
+// evento botão fechar
+cancel.addEventListener(
+    "click",
+    fechar
+);
+
+// foreach- percorre todos os botões 
+criar.forEach(
+    function(botao){
+        botao.addEventListener(
+            "click",
+            abrir
+        );
+    }
+);
+
+// clique de fundo do modal
+modal.addEventListener(
+    "click",
+    function(evento){
+        if(evento.target === modal)
+            fechar();
+    }
+);
