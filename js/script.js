3d929890938921b95e2dc8d999e450907141580e
Strangehell64 <joaovictorsantossilva4345@gmail.com>
2026-07-27 16:54:44 -0300

    debugando

--- js/script.js
+++ js/script.js
@@ -13,6 +13,8 @@ function logbt() {
         alert("usuario incorreto, tente novamente");
     }
 }
+
+
 // variavel modal
 const modal = document.querySelector(".container-modal");
 // moldura modal
@@ -22,35 +24,3 @@ const cancel = document.querySelector("#cancel")
 // criar tarefa
 const criar = document.querySelectorAll("#adicao")
 // Ação do modal
-function criar() {
-    // estilo para modal
-    modal.style.display = "flex";
-}
-// esconder modal
-function cancel() {
-    modal.style.display = "none";
-}
-// evento botão fechar
-cancel.addEventListener(
-    "click",
-    fechar
-);
-
-// foreach- percorre todos os botões 
-criar.forEach(
-    function(botao){
-        botao.addEventListener(
-            "click",
-            abrir
-        );
-    }
-);
-
-// clique de fundo do modal
-modal.addEventListener(
-    "click",
-    function(evento){
-        if(evento.target === modal)
-            fechar();
-    }
-);
